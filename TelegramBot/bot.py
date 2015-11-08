4452cb6b05b0a263f491f1a094b98ffde823b3c3
rahul2308 <rahul2308@outlook.com>
2015-11-09 05:26:39 +0530

    Updated workflow of the bot.

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -4,6 +4,7 @@ from token import TOKEN
 
 
 LAST_UPDATE_ID = None
+ans_stage = 0
 
 
 def main():
@@ -28,6 +29,7 @@ def main():
 
 def echo(bot):
     global LAST_UPDATE_ID
+    global ans_stage
 
     # Request updates after the last updated_id
     for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=10):
@@ -43,14 +45,75 @@ def echo(bot):
         if command == '/reverse':
             message = Str[::-1]
 
-        if (message):
-            # Reply the message
-            print str(chat_id) + ' :: ' + str(message)
-            bot.sendMessage(chat_id=chat_id,
-                            text=message)
-
-            # Updates global offset to get the new updates
+        if command == '/start':
+            custom_keyboard = [[ telegram.Emoji.THUMBS_UP_SIGN, telegram.Emoji.THUMBS_DOWN_SIGN ]]
+            reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+            bot.sendMessage(chat_id=chat_id, text="You want to register a complaint?", reply_markup = reply_markup)
             LAST_UPDATE_ID = update.update_id + 1
+            ans_stage = 1
+            continue
+
+        print str(chat_id) + ' :: ' + message
+
+        if message:
+            if ans_stage == 1:
+                # Reply the message
+                # if message == 'yes':
+                #     bot.sendMessage(chat_id=chat_id, text="Okay")
+                # else:
+                #     bot.sendMessage(chat_id=chat_id, text="Thanks for your interest")
+                # bot.sendMessage(chat_id=chat_id,
+                #                 text=message)
+                if message == telegram.Emoji.THUMBS_UP_SIGN:
+                    bot.sendMessage(chat_id=chat_id, text="Choose Category.\n1.Pothole\n2.Sewage Problem.", reply_markup = telegram.ReplyKeyboardHide())
+                    ans_stage += 1
+                elif message == telegram.Emoji.THUMBS_DOWN_SIGN:
+                    bot.sendMessage(chat_id=chat_id, text="Thanks for your interest.\n Type /start to start over.", reply_markup = telegram.ReplyKeyboardHide())
+                else:
+                    custom_keyboard = [[ telegram.Emoji.THUMBS_UP_SIGN, telegram.Emoji.THUMBS_DOWN_SIGN ]]
+                    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+                    bot.sendMessage(chat_id=chat_id, text="Oops, wrong input. Try again.", reply_markup = reply_markup)
+
+            elif ans_stage == 2:
+                if int(message) < 1 or int(message) > 2:
+                    bot.sendMessage(chat_id=chat_id, text="Oops, wrong input. Try again.")
+                else:
+                    #add the category in database
+                    custom_keyboard = [[ telegram.Emoji.THUMBS_UP_SIGN, telegram.Emoji.THUMBS_DOWN_SIGN ]]
+                    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+                    bot.sendMessage(chat_id=chat_id, text="Do want to be anonymous?", reply_markup = reply_markup)
+                    ans_stage += 1
+                # LAST_UPDATE_ID = update.update_id + 1
+            elif ans_stage == 3:
+                if not(message == telegram.Emoji.THUMBS_UP_SIGN or message == telegram.Emoji.THUMBS_DOWN_SIGN):
+                    custom_keyboard = [[ telegram.Emoji.THUMBS_UP_SIGN, telegram.Emoji.THUMBS_DOWN_SIGN ]]
+                    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+                    bot.sendMessage(chat_id=chat_id, text="Oops, wrong input. Try again.", reply_markup = reply_markup)
+                #add the anonymity in the databse here
+                bot.sendMessage(chat_id=chat_id, text="Attach a photo.", reply_markup = telegram.ReplyKeyboardHide())
+                ans_stage += 1
+                # LAST_UPDATE_ID = update.update_id + 1
+            elif ans_stage == 4:
+                #add the photo in the database
+                bot.sendMessage(chat_id=chat_id, text="Write comment(optional). Please reply 'No' if not interested")
+                ans_stage += 1
+                # LAST_UPDATE_ID = update.update_id + 1
+
+            elif ans_stage == 5:
+                #add the comment in the database
+                bot.sendMessage(chat_id=chat_id, text="Please send us the location so that we could detect it.")
+                ans_stage += 1
+                # LAST_UPDATE_ID = update.update_id + 1
+
+            elif ans_stage == 6:
+                #add the location in the database
+                bot.sendMessage(chat_id=chat_id, text="Thanks for your co-operation. Have a nice day.")
+                ans_stage += 1
+
+        # Updates global offset to get the new updates
+        LAST_UPDATE_ID = update.update_id + 1
+
+            
 
 
 if __name__ == '__main__':
